a980052ec8abdcd5bd24fe85859e9276f75ae68b
Noah <64989000+NoahLaHaise@users.noreply.github.com>
2026-02-17 22:08:16 -0500

    telegram updates

--- python/Telegram/TelegramMessenger.py
+++ python/Telegram/TelegramMessenger.py
@@ -7,15 +7,24 @@ load_dotenv()
 class TelegramMessenger:
     message: str
     endpoint: str
+    chat_id: int
 
-    def __init__(self, msg: str):
-        self.message = msg
+    def __init__(self):
         self.endpoint = os.getenv("TELEGRAM_ENDPOINT")
+        self.chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     
-    def send_message(self):
-        url = self.endpoint + self.message
+    def send_message(self, msg: str):
+        #print(self.endpoint)
+        url = f'{self.endpoint}/sendMessage?chat_id={self.chat_id}&text={msg}'
         requests.post(url)
 
-tele = TelegramMessenger('HEYYYYYYYYYY')
-tele.send_message()
+    def get_messages(self):
+        url = f'{self.endpoint}/getUpdates'
+        response = requests.get(url)
+        print(response.text)
+
+
+tele = TelegramMessenger()
+tele.send_message('test')
+tele.get_messages()
